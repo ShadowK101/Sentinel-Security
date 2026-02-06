68a8831c869214c79f0de9b1e396e17239fd62c8
Keegan Mostert <mostertkeegan05@gmail.com>
2026-02-06 07:48:42 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1321013034-a4a1f",
+  "appId": "1:411920958477:web:f2e7a2d8ad4e3ffed14144",
+  "apiKey": "AIzaSyDHVfQ1VE2kGtwMqz87imi3VkOK5gTfGd0",
+  "authDomain": "studio-1321013034-a4a1f.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "411920958477"
+};
