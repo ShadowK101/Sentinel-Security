072a86615cdc159113fc082f143e8afa29f6e184
Keegan Mostert <mostertkeegan05@gmail.com>
2026-02-06 08:31:04 +0000

    make a animation for when teh notifcaion disspaears because rn it just k

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -88,10 +88,30 @@ export default {
             height: '0',
           },
         },
+        "in-from-right": {
+          "0%": { transform: "translateX(100%)" },
+          "100%": { transform: "translateX(0)" },
+        },
+        "out-to-right": {
+          "0%": { transform: "translateX(0)" },
+          "100%": { transform: "translateX(100%)" },
+        },
+        "fade-in": {
+          "0%": { opacity: "0" },
+          "100%": { opacity: "1" },
+        },
+        "fade-out": {
+          "0%": { opacity: "1" },
+          "100%": { opacity: "0" },
+        }
       },
       animation: {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
+        "in-from-right": "in-from-right 0.3s ease-out",
+        "out-to-right": "out-to-right 0.3s ease-in",
+        "fade-in": "fade-in 0.2s ease-out",
+        "fade-out": "fade-out 0.2s ease-in",
       },
     },
   },
