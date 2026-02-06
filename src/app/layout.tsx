0170ff165a27b00c32a22d2fdbdefe66d661c3b6
Keegan Mostert <mostertkeegan05@gmail.com>
2026-02-06 10:25:29 +0000

    feat: add google site verification

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,6 +23,7 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet" />
+        <meta name="google-site-verification" content="aJppyP_kgrYobOFgec4u4SG7JZL7rwi_CtwABKUwMhs" />
       </head>
       <body className="font-body antialiased">
         <FirebaseClientProvider>
